f8ba361302727bd34dbabff0b75724ede72bf6ae
Yevhen <ewgenator2014@gmail.com>
2018-11-22 20:57:08 +0200

    checkit
    lab

---
+++ js/app.js
@@ -0,0 +1,8 @@
+function hide_form() {
+	document.getElementById("forma_id").style.display = "none";
+	document.getElementById("show_button").style.display = "flex";
+}
+function show_form() {
+	document.getElementById("forma_id").style.display = "flex";
+	document.getElementById("show_button").style.display = "none";
+}
